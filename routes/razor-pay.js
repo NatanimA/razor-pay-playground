5559be9abf0f2c096c48e397470b4105cfa83d59
NatanimA <Natanimabesha@gmail.com>
2023-05-11 18:58:51 +0300

    Razor Pay Api Subscriptions and Plans

--- routes/razor-pay.js
+++ routes/razor-pay.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import { razorPayInstance } from "../constants/index.js";
 
 
@@ -33,6 +33,74 @@ razorRouter.post("/payments/order", async (req,res ) => {
         return res.json({err})
     }
 })
+
+
+razorRouter.route("/payments/plan")
+    .post((req,res) => {
+        const {amount,period,name,currency,description,notes} = req.body;
+        try{
+            razorPayInstance.plans.create({
+                period:period,
+                interval: 1,
+                item:{
+                    name,
+                    amount,
+                    currency,
+                    description
+                },
+                notes
+
+            },(error,response) => {
+                if(!error) return res.json({response})
+                return res.json({error})
+            })
+        }catch(err){
+            return res.json({err})
+        }
+    })
+
+    .get( async (_,res) => {
+        try {
+            const response = await razorPayInstance.plans.all()
+            return res.json({response})
+        } catch (error) {
+            return res.json({error})
+        }
+    });
+
+razorRouter.get("/payments/plans/:id", (req,res) => {
+    try {
+        const {id} = req.params;
+
+        razorPayInstance.plans.fetch(id,(error,response) => {
+            if(!error) return res.json({response})
+            return res.json({error})
+        })
+    } catch (error) {
+        return res.json({error})
+    }
+})
+
+
+razorRouter.route("/payments/subscription")
+
+    .post((req,res) => {
+        try {
+            const {plan_id,quantity,total_count} = req.body;
+            razorPayInstance.subscriptions.create({
+                plan_id,
+                customer_notify:1,
+                quantity,
+                total_count
+            },(error,response) => {
+                if(!error) return res.json({response})
+                return res.json({error})
+            })
+        } catch (error) {
+            return res.json({error})
+        }
+    })
+
 
 
 export default razorRouter;
